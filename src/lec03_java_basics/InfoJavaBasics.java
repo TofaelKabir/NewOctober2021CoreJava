59ab805ef3dd4452d0bf017ed006e1c40808ce68
tofaelKabir <tofaelkabir@gmail.com>
2021-10-10 22:16:44 -0400

    10102021 10:16pm

--- src/lec03_java_basics/InfoJavaBasics.java
+++ src/lec03_java_basics/InfoJavaBasics.java
@@ -2,12 +2,13 @@ package lec03_java_basics;
 
 /*
  * 
-1) What is Java Reserved words?
+
+1) What are Java Reserved words?
 -- In Java, there are keywords that are reserved for the use of Java functions or other uses that cannot be identifiers like variables, classes and function names. When a reserved word is used as a variable, we will get an error or some other unexpected result. The following are more examples of reserved words.
 
 abstract, if, private, this, double, implements, throw, boolean, else, import, public, throws, break, return, byte, extends, int, short, true, false, case, interface, static, try, catch, final, long, void.
 
-2) How to change font size for syntax permanently?
+2) How to change the font size for syntax permanently?
 Preferences -- General --- Appearance ----- Colors and Fonts ------ Scroll and find Text Fonts --- double click --- choose Collection,  Family, Typeface, Size (Choose font size) --- cross the page --- Apply -- Apply and close
 
 3) how to see the font bigger? how to smaller (temporarily)
@@ -15,10 +16,10 @@ Preferences -- General --- Appearance ----- Colors and Fonts ------ Scroll and f
 -- Command/control -
 
 4) How to Enable Tasks in Eclipse IDE?
--- It is used to find out, what kind of work is left behind and need attention.
-Window -- Show View -- Tasks
+-- It is used to find out what kind of work is left behind and needs attention.
+To see - go to --> Window (eclipse header) -- Show View -- Tasks
 We have to Use 
-//TODO ----------- write what need to do in future -------
+//TODO ----------- write what need to do in future, or any important instruction to follow in future -------
 
 5) How to remove some part of code or a method from SOMEwhere to ANYWhere?
 -- Select that part, then alt/option + up/down arrow sign
@@ -27,7 +28,7 @@ We have to Use
 -- Select, then shift + Command/control + x
 -- Select, then shift + Command/control +  y (to turn it back where it was)
 
-7) How to turn a upper case string to lower case string?
+7) How to turn an upper case string to lowercase string?
 -- Select, then shift + Command/control + y
 -- Select, then shift + Command/control +  x (to turn it back where it was)
 
@@ -42,14 +43,14 @@ We have to Use
 -- https://www.baeldung.com/java-string-newline#:~:text=In%20Windows%2C%20a%20new%20line,the%20end%20of%20our%20string.
 
 11) What is a carriage return Java? 
-// TODO WE HAVE TO FINDOUT /r in details
+// TODO WE HAVE TO FIND OUT /r in details
 -- In Windows, a new line is denoted using “\r\n”, sometimes called a Carriage Return and Line Feed, or CRLF. Adding a new line in Java is as simple as including “\n” , “\r”, or “\r\n” at the end of our string.
 -- // https://www.codespeedy.com/how-does-a-carriage-return-r-works-in-java/
 
 12) How to refactor/rename?
--- right click on Class/package, select refactor, the rename, change it accordingly. Please don't change Project name.
+-- right click on Class/package, select refactor, then rename, change it accordingly. Please don't change the Project name.
 
-13) what kind of mistake is common?
+13) What kind of mistake is common?
 -- missing of curly brace, missing of ; missing of ""
 
 14) How to print "Tofael"?
@@ -87,7 +88,7 @@ i) right click on class -- select copy -- go to package -- paste
 ii) select class -- control/command+c -- go to package --- control/command+v
 iii) select the class -- drag it -- drop it to the package
 
-20) How to drag and drop of a class from one package to another?
+20) How to drag and drop a class from one package to another?
 --select the class -- drag it -- drop it to the package
 
 21) camelcase vs underscore
